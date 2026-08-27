91d267c073f7707854db104acf84f03c20a4bd48
温州程序员劝退师 <adaihahaha@hotmail.com>
2026-08-27 15:08:37 +0800

    fix: enable Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://chixitown.com',
+  site: 'https://blog.chixitown.com',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
